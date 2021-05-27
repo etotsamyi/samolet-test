c3e7f6d62195de2c0cb24777fb6a6de614e78a1e
unknown <kindcat1101@gmail.com>
2021-05-28 01:38:48 +0300

    some chages

--- src/Pages/InnerCard.tsx
+++ src/Pages/InnerCard.tsx
@@ -1,5 +1,4 @@
-//@ts-nocheck
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useStore } from 'effector-react';
 import { dataset } from '../store/store';
 import { IDataItem } from '../types/IDataItem';
@@ -12,10 +11,19 @@ interface IProps {
 
 export const InnerCard: FC<IProps> = ({ match }) => {
     const order = match.params.order;
-    const card = useStore(dataset).find((element: IDataItem) => element.order === Number(order));
-    console.log(card);
+    const data: IDataItem[] = useStore(dataset)
+    const index: number = data.findIndex((element: IDataItem) => element.order === Number(order));
+    const card = data[index]
 
     return !!card ? <div className="inner-card">
-
+        <span>{card.territory}</span>
+        <span>{card.users}</span>
+        <span>{card.site}</span>
+        <span>{card.address}</span>
+        {/* <span>{cardData.territory}</span>
+        <span>{cardData.territory}</span>
+        <span>{cardData.territory}</span>
+        <span>{cardData.territory}</span>
+        <span>{cardData.territory}</span> */}
     </div> : <div className="inner-card">Ошибка</div>
 }
